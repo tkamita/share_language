4a7a433baf3f9c4c2a4447c7a567e1c2967210a0
嘉味田崇 <[kamitatakashi@gmail.com]>
2020-10-06 21:16:48 +0900

    fix search

--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -6,21 +6,36 @@ use Illuminate\Http\Request;
 use App\Residence;
 use App\Feature;
 use App\Prefecture;
+use App\Image;
 
 class SearchController extends Controller
 {
     public function search(Request $request) {
         // $keyword = $request->input('keyword');
         $search = $request->all();
-        $query = Residence::query();
+        // $query = Residence::query();
+        // $query = Residence::query();
+        // ->join('prefectures', function ($query) use ($request) {
+        //     $query->on('residences.prefecture_id', '=', 'prefectures.id');
+        // // })->join('features', function ($query) use ($request) {
+        // //     $query->on('residences.feature_id', '=', 'features.id');
+        // });
+        // })->join('images', 'images.residence_id', '=', 'residence_id');
+        // $query = Residence::join('prefectures', 'residences.prefecture_id', '=', 'prefectures.id')
+        //                     ->join('residence_features', 'residences.id', '=', 'residence_features.residence_id');
+                            // ->query();
         // $query->join('prefectures', function ($query) use ($request) {
         //     $query->on('residences.prefecture_id', '=', 'prefectures.id');
         // });
         // $query->join('features', function ($query) use ($request) {
         //     $query->on('residences.feature_id', '=', 'features.id');
         // });
+        // $query->whereHas('prefectures', function($query){
+        //     $query->where('name', 'LIKE', "%{$request->keyword}%");
+        // });
         if(!empty($search)) {
-            $query->where('name', 'LIKE', "%{$request->keyword}%")
+            $query = Residence::where('residences.name', 'LIKE', "%{$request->keyword}%")
+            // $query->where('residences.name', 'LIKE', "%{$request->keyword}%")
                   ->orWhere('location', 'LIKE', "%{$request->keyword}%")
                   ->orWhere('introduction', 'LIKE', "%{$request->keyword}%");
                 //   ->orWhere('minimum_rent', '>=', $request->minimum_rent)
@@ -28,10 +43,22 @@ class SearchController extends Controller
                 //   ->orWhere('common_charge', '<=', $request->common_charge)
                 //   ->orWhere('term', '<=', $request->term)
                 //   ->orWhere('room_num', '>=', $request->room_num)
-                //   ->orWhere('prefectures.name', 'LIKE', "%{$request->keyword}%")
+                //   ->orWhere('prefectures.name', 'LIKE', "%{$request->keyword}%");
                 //   ->orWhere('content', 'LIKE', "%{$request->keyword}%");
+                //   ->whereHas('prefectures', function($query){
+                //       $query->where('name', 'LIKE', "%{$request->keyword}%")
+                //   });
+                //   ->whereHas('features', function($query){
+                //       $query->where('content', 'LIKE', "%{$request->keyword}%")
+                //   });
+        }
+        if(!empty($request->minimum_rent)) {
+            $query->where('minimum_rent', '>=', $request->minimum_rent);
         }
         $residences = $query->get();
+        // $residences = $query->join('images', 'images.residence_id', '=', 'residences.id')->get();
+        // dd($residences);
+
         $params = [
             'residences' => $residences,
         ];
