95ef704ae16da36593de4231ac88478b92031d80
嘉味田崇 <[kamitatakashi@gmail.com]>
2020-10-10 18:19:19 +0900

    fix about view

---
+++ resources/views/about.blade.php
@@ -0,0 +1,26 @@
+@extends('layouts.app')
+@section('content')
+<div class="container mt-5">
+  <div class="row justify-content-center">
+    <div class="mt-5 mb-3 pb-5 border-bottom col-md-8">
+      <h3 class="text-center font-weight-bold">暮らして学ぶ</h3>
+      <h3 class="text-center font-weight-bold">英語を学ぶシェアハウス</h3>
+      <div class="mx-auto mt-3" style="width: 400px;">
+        <p class="text-center">Share Languageは英語を学びたいと考えている人が日本語を学びたいと考えている外国人とシェアハウスで一緒に暮らし、英語力をのばして行くコミュニティの事です。定期的に開催される英会話レッスンやアクティビティを通じて日常生活で英語学習をする事ができます。</p>
+      </div>
+    </div>
+  </div>
+  <div class="row justify-content-center">
+    <div class="mt-5 border-bottom col-md-8">
+      <h3 class="text-center font-weight-bold">一緒に居るのが楽しい。</h3>
+      <h3 class="text-center font-weight-bold">楽しいから</h3>
+      <h3 class="text-center font-weight-bold">一緒にすみたい。</h3>
+      <div class="mx-auto mt-3" style="width: 400px;">
+        <p class="text-center mb-4">物件は、キッチンやリビングなどを複数のテナントで「シェア（共有）」することで住空間の効率化を計り、より価値の高い住宅を抑えた価格でご提供できるようにした物件です。</p>
+        <p class="text-center mb-4">もちろん自分だけの「個室」でプライベートはしっかり確保しています。基本的な家電や家具、雑貨類は予めご用意してあり、初期費用が安いので、上京されて来られる方や、新たに一人暮らしを始められる方でも、手軽に東京での生活を始めていただけます。</p>
+        <p class="text-center mb-4">より広いリビングや設備の整ったキッチン。大型液晶テレビやインターネット環境。「シェア（共有）」した分だけ、生活スペースが増えるとお考え下さい。そして安全である事に加え、人と触れ合う機会が増えるというのも、ワンルーム暮らしにはないポイントです。</p>
+      </div>
+    </div>
+  </div>
+</div>
+@endsection
